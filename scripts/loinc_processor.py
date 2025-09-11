2c7ea7e4679e5e406ec80bb5bf79e61c193ab931
Emily Gallegos <emilygallegos@MacBook-Pro.local>
2025-09-11 16:31:30 -0400

    loinc_small file completed

--- scripts/loinc_processor.py
+++ scripts/loinc_processor.py
@@ -14,10 +14,12 @@ loinc_small = loinc[['LOINC_NUM', 'LONG_COMMON_NAME']]
 loinc_small['last_updated'] = '2025-09-01'
 loinc_small = loinc_small.rename(columns={
     'LOINC_NUM': 'code', 
-    'LONG_COMMON_NAME': 'description' , 
-    'last_updated' : 'last_udated'
+    'LONG_COMMON_NAME': 'description', 
+    'last_updated' : 'last_updated'
     })
 
 file_output_path = 'output/loinc_small.csv'
 
-loinc_small.to_csv(file_output_path) 
+loinc_small.to_csv(file_output_path, index=False) 
+
+print(f"loinc_small data saved to {file_output_path}")
